1393f92496ad4a5dda9e7132ec0a0a42a3e5c523
ImFatF1sh <zijianjiao2017@gmail.com>
2021-04-09 21:15:11 +0800

    fixup! dandelion: Correct build description
    * This also fix misound err in logcat
    
    Traceback (most recent call last):
      File "./build/tools/releasetools/sign_target_files_apks", line 1233, in <module>
        main(sys.argv[1:])
      File "./build/tools/releasetools/sign_target_files_apks", line 1214, in main
        compressed_extension)
      File "./build/tools/releasetools/sign_target_files_apks", line 493, in ProcessTargetFiles
        new_data = RewriteProps(data.decode())
      File "./build/tools/releasetools/sign_target_files_apks", line 690, in RewriteProps
        assert len(pieces) == 5
    AssertionError
    
    Signed-off-by: ImFatF1sh <zijianjiao2017@gmail.com>
    Change-Id: I1ab5ae41771f14e5421542a02f30474297d984ff
    Signed-off-by: ImFatF1sh <zijianjiao2017@gmail.com>

--- lineage_dandelion.mk
+++ lineage_dandelion.mk
@@ -36,7 +36,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := dandelion
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dandelion-user 10 QP1A.190711.020 release-keys"
+    PRIVATE_BUILD_DESC="dandelion-user 10 QP1A.190711.020 V12.0.11.0.QCDMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := google/sunfish/sunfish:11/RQ2A.210305.006/7119741:user/release-keys
