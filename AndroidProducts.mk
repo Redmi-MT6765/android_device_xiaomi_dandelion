5833a4330646bd7b70da6d6d91354148b1102313
ImFatF1sh <zijianjiao2017@gmail.com>
2021-04-25 20:13:32 +0800

    dandelion: Clean "No newline at the end of file"
    Signed-off-by: ImFatF1sh <zijianjiao2017@gmail.com>
    Change-Id: Ic61667dc6612c8628a2243df2269a16e48b3d3bb

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,5 @@ PRODUCT_MAKEFILES := \
    $(LOCAL_DIR)/lineage_dandelion.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dandelion-user \
     lineage_dandelion-userdebug \
     lineage_dandelion-eng
